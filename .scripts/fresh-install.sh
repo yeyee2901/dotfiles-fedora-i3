d951788bf25e799d0d09ce00d5f975a77db76085
yeyee2901 <gabriel777sh@gmail.com>
2021-11-14 17:14:14 +0700

    EDIT: fresh install

--- .scripts/fresh-install.sh
+++ .scripts/fresh-install.sh
@@ -11,6 +11,12 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -o $HOME/ohmyzsh-install.sh
 chmod +x $HOME/ohmyzsh-install.sh
 
+# ZSH syntax highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
+
 # Install rust things
 # Rust toolchain
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+# cargo install exa bat
+
